a4434490c2e5cfb0695d7cecce73f671a892f4db
marcadnin <m_adnin@etu-webschoolfactory.fr>
2019-01-14 00:50:31 +0100

    indentation

--- code.js
+++ code.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
+  // Un clic sur le bouton "oui" affiche le formulaire de recherche du nom d'un infirmier et cache le reste
+  $(".oui").on("click", function() {
+    $(".connu").transition("fade");
+    $(".chercher, .accueil").hide();
+  });
+  // Un clic sur le bouton "oui" affiche le formulaire de recherche général d'un infirmier et cache le reste
+  $(".non").on("click", function() {
+    $(".chercher").transition("fade");
+    $(".connu, .accueil").hide();
+  });
 
-    // Un clic sur le bouton "oui" affiche le formulaire de recherche du nom d'un infirmier et cache le reste
-    $( ".oui" ).on( "click", function() {
-        $( ".connu" ).transition('fade')
-        $( ".chercher, .accueil" ).hide();
-
-    });
-    // Un clic sur le bouton "oui" affiche le formulaire de recherche général d'un infirmier et cache le reste
-    $( ".non" ).on( "click", function() {
-        $( ".chercher" ).transition('fade');
-        $( ".connu, .accueil" ).hide();
-    });
-
-// Initialise le formulaire
+  // Initialise le formulaire
   $(".ui.dropdown").dropdown();
   $(".ui.radio.checkbox").checkbox();
 
@@ -74,120 +72,117 @@ $(document).ready(function() {
     }
   });
 
-
-  $('.ui.form.chercher')
-  .form({
-      // Verification des champs du formulaire, si l'un d'entre eux n'est pas correct un message apparaitra et le background du champ sera rouge
+  $(".ui.form.chercher").form({
+    // Verification des champs du formulaire, si l'un d'entre eux n'est pas correct un message apparaitra et le background du champ sera rouge
     fields: {
       codepostal: {
-        identifier: 'codepostal',
+        identifier: "codepostal",
         rules: [
           {
-            type   : 'empty',
-            prompt : 'Merci d\'entrer votre code postal'
+            type: "empty",
+            prompt: "Merci d'entrer votre code postal"
           },
           {
-            type   : 'exactLength[5]',
-            prompt : 'Merci d\'entrer un code postal valide'
+            type: "exactLength[5]",
+            prompt: "Merci d'entrer un code postal valide"
           }
         ]
       },
       soins: {
-        identifier: 'soins',
+        identifier: "soins",
         rules: [
-            {
-                type   : 'empty',
-                prompt : 'Merci de choisir au moins une catégorie de soins'
-              },
           {
-            type   : 'maxCount[3]',
-            prompt : 'Merci de choisir au maximum 3 soins'
+            type: "empty",
+            prompt: "Merci de choisir au moins une catégorie de soins"
+          },
+          {
+            type: "maxCount[3]",
+            prompt: "Merci de choisir au maximum 3 soins"
           }
         ]
       },
       email: {
-        identifier: 'email',
+        identifier: "email",
         rules: [
           {
-            type   : 'empty',
-            prompt : 'Merci d\'entrer votre adresse e-mail'
+            type: "empty",
+            prompt: "Merci d'entrer votre adresse e-mail"
           },
           {
-            type   : 'email',
-            prompt : 'Merci d\'entrer une adresse e-mail valide.'
+            type: "email",
+            prompt: "Merci d'entrer une adresse e-mail valide."
           }
         ]
-      },
+      }
     },
     // Si le formulaire est correct, une suite d'instructions se lance
-    onSuccess:function(){
-        // Récupération des valeurs de préférence de genre et de soins nécessaires pour le patient
-        let genre = $('#preference').find('[name="genre"]:checked').val();
-        let soins = $('.ui.dropdown').dropdown('get value');
-        // Conversion du résultat de la préférence de genre d'un string à un booléen
-        var genre_verif = checkBoolean(genre);
-        // Initialisation du compteur, tant qu'il reste à zéro l'infirmier conviendra au patient
-        var compteur = 0;
-        // Initialisation du contenu de la modale d'affichage des profils d'infirmiers
-        var modale = "";
-        // Boucle for pour parcourir le tableau d'infirmiers
-        for (var i = 0; i < contenu.length; i++) {
-            // Réinitialisation du compteur à chaque boucle (pour chaque infirmier)
-            compteur = 0;
-            // Vérification du genre par une condition
-            if (contenu[i].female == genre_verif){
-                    // Deuxième boucle for pour comparer chaque type de soin demandé aux compétences des infirmiers 
-                    for (var y = 0; y < soins.length; y++) {
-                        // Si un soin n'est pas proposé par un infirmier
-                        if (contenu[i].knowHows.includes(soins[y]) == false){
-                            // Le compteur passe au dessus de zéro
-                            compteur++;
-                        }
-                    }
-                    // On ne garde ici donc que les infirmiers qui conviennent au patient
-                    if (compteur == 0){
-                        // Le code html présentant chaque infirmier est ajouté à la variable modale
-                        modale = modale +(`
+    onSuccess: function() {
+      // Récupération des valeurs de préférence de genre et de soins nécessaires pour le patient
+      let genre = $("#preference")
+        .find('[name="genre"]:checked')
+        .val();
+      let soins = $(".ui.dropdown").dropdown("get value");
+      // Conversion du résultat de la préférence de genre d'un string à un booléen
+      var genre_verif = checkBoolean(genre);
+      // Initialisation du compteur, tant qu'il reste à zéro l'infirmier conviendra au patient
+      var compteur = 0;
+      // Initialisation du contenu de la modale d'affichage des profils d'infirmiers
+      var modale = "";
+      // Boucle for pour parcourir le tableau d'infirmiers
+      for (var i = 0; i < contenu.length; i++) {
+        // Réinitialisation du compteur à chaque boucle (pour chaque infirmier)
+        compteur = 0;
+        // Vérification du genre par une condition
+        if (contenu[i].female == genre_verif) {
+          // Deuxième boucle for pour comparer chaque type de soin demandé aux compétences des infirmiers
+          for (var y = 0; y < soins.length; y++) {
+            // Si un soin n'est pas proposé par un infirmier
+            if (contenu[i].knowHows.includes(soins[y]) == false) {
+              // Le compteur passe au dessus de zéro
+              compteur++;
+            }
+          }
+          // On ne garde ici donc que les infirmiers qui conviennent au patient
+          if (compteur == 0) {
+            // Le code html présentant chaque infirmier est ajouté à la variable modale
+            modale =
+              modale +
+              (`
                         <div class="item">
                             <div class="ui small circular image">
-                                <img src="images/`+ contenu[i].picture + `">
+                                <img src="images/` + contenu[i].picture + `">
                             </div>
                             <div class="middle aligned content">
-                                <h2 class="ui pink header">`+ contenu[i].title + `</h2>
+                                <h2 class="ui pink header">` + contenu[i].title + `</h2>
                             </div>
                         </div>
-                    `)
-                    }
-            }
-            
+                    `);
           }
-        // Si la variable modale contient quelque chose (une liste d'infirmiers donc)
-        if (modale){
+        }
+      }
+      // Si la variable modale contient quelque chose (une liste d'infirmiers donc)
+      if (modale) {
         // La variable modale est injectée sur la page html dans la moale prévue
         $(".ui.items").html(modale);
         // Et elle est affichée
         $(".ui.basic.modal.liste").modal("show");
-        } else {
-            // Si la variable modale est vide, la modale d'indisponibilité d'infirmiers s'affiche
-            $(".ui.modal.indisponible").modal("show");
-        }
-        // Permet d'éviter un rafraichissement automatique de la page à cause du formulaire
-        return false;
+      } else {
+        // Si la variable modale est vide, la modale d'indisponibilité d'infirmiers s'affiche
+        $(".ui.modal.indisponible").modal("show");
+      }
+      // Permet d'éviter un rafraichissement automatique de la page à cause du formulaire
+      return false;
     }
-  }
-    
-    
-   
-   );
+  });
 
-    // Fonction de conversion string vers booléen
-   function checkBoolean(value){
+  // Fonction de conversion string vers booléen
+  function checkBoolean(value) {
     if (value === "true") {
-        return true;
+      return true;
     }
 
     if (value === "false") {
-        return false;
+      return false;
     }
-   }
+  }
 });
